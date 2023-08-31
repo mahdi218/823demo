a6ce6a14c07c715d9d37faad2f79942ea16d0546
mahdi218 <mahdi_218@yahoo.com>
2023-08-31 00:29:36 -0400

    singleton

---
+++ painting/src/main/java/net/zeroone/StringSingleton.java
@@ -0,0 +1,19 @@
+package net.zeroone;
+
+
+public class StringSingleton {
+    /**
+     * a static variable is shared across the entire application
+     * 
+     */
+    public static String data;
+
+    /**
+     * Let's say that we need to distribute the exact same string across our entire app.
+     * But, we only need to set the string value once! we don't want to re-set it.
+     * 
+     * If this were a DB Connection, we don't want to kill the connection when the connection is 
+     * 
+     * 
+     */
+}
